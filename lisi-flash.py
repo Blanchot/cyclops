7db7e0ecb16305253dbdc307eb3f0ba35d88059b
Blanchot <Blanchot@users.noreply.github.com>
2018-08-04 16:19:46 +0200

    Testing...

--- lisi-flash.py
+++ lisi-flash.py
@@ -9,6 +9,15 @@ GPIO.setwarnings(False)
 GPIO.setup(4, GPIO.OUT)
 GPIO.output(4, GPIO.LOW)
 
+def flashon():
+  time.sleep(5)
+  GPIO.output(4, GPIO.HIGH)
+  print('Flash on')
+  time.sleep(10)
+  GPIO.output(4, GPIO.LOW)
+  print('Flash off')
+
+
 def tidyup():
   GPIO.cleanup()
 
